a8a2bfeeaeaa18567e3f567ce1520576b67e53ee
zthking <tzhang017@gmail.com>
2019-04-13 16:14:09 -0400

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,13 @@
+Public class HelloNumbers {
+	public static void main(string[] args){
+		int x = 0;
+		int i = 0;
+		while (i < 10) {
+			x = x + i;
+			System.out.print(x);
+			i = i + 1;
+		}
+	}
+}
+
+	

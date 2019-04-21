605e54f14508cb5097d74c02e5d720536ab6f732
zthking <tzhang017@gmail.com>
2019-04-20 20:02:12 -0400

    added NBody.java

---
+++ proj0/NBody.java
@@ -0,0 +1,10 @@
+public class NBody{
+	public double readRadius{
+
+		In in = new In(String link);
+		int firstItemInFile = in.readInt();
+		double secondItemInFile = in.readDouble();
+
+		return secondItemInFile;
+	}
+}

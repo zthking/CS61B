39f11ce93242a6e3baf461c4fb4894617a96e56b
zthking <tzhang017@gmail.com>
2019-04-22 22:46:55 -0400

    completed readBodies

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,17 +1,27 @@
 public class NBody{
 
-/*
-	public static void main(String arg []){
-
-	}
-*/
 	public static double readRadius(String link){
+
+        In in = new In(link);
+		int firstItemInFile = in.readInt();
+		double secondItemInFile = in.readDouble();
+
+		return secondItemInFile;
+	}
+
+	public static Body[] readBodies(String link){
 
 		In in = new In(link);
 		int firstItemInFile = in.readInt();
 		double secondItemInFile = in.readDouble();
 
-		return secondItemInFile;
+		Body[] allPlanets = new Body[5];
+		for (int row = 0; row < 5; row++){
+
+				allPlanets[row] = new Body(in.readDouble(), in.readDouble(), in.readDouble(), 
+					in.readDouble(), in.readDouble(), in.readString());
+		}
+		return allPlanets;
 	}
 
 }

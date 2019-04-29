4643d8aef2e53b9329928928b27c3663589c8898
zthking <tzhang017@gmail.com>
2019-04-29 15:30:19 -0400

    Revised IntList to handle null situation.

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,9 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null && B == null) {
+            return null;
+        }
         IntList C = A;
         while (C.rest != null) {
             C = C.rest;
@@ -94,6 +97,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if (A == null && B == null) {
+            return null;
+        }
         IntList temp = new IntList(A.first, null);
         IntList ptr = temp;
         A = A.rest;

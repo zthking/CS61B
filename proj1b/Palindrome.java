0b823085aa2d3f21d9c991e25e85111f29080606
zthking <tzhang017@gmail.com>
2019-05-18 11:40:48 -0400

    Complete first version of Palindrome.java

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,2 +1,49 @@
 public class Palindrome {
+
+    public Deque<Character> wordToDeque(String word) {
+
+        Deque<Character> charValue = new Deque<>() {
+            @Override
+            public void addFirst(Character item) {
+
+            }
+
+            @Override
+            public void addLast(Character item) {
+
+            }
+
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public void printDeque() {
+
+            }
+
+            @Override
+            public Character removeFirst() {
+                return null;
+            }
+
+            @Override
+            public Character removeLast() {
+                return null;
+            }
+
+            @Override
+            public Character get(int index) {
+                return null;
+            }
+        };
+
+        for (int i = 0; i < word.length(); i++) {
+            char temp = word.charAt(i);
+            charValue.addLast(temp);
+        }
+
+        return charValue;
+    }
 }

acdc6d9aa1a067a2544b808bb6abe0c90552660f
zthking <tzhang017@gmail.com>
2019-05-18 12:13:02 -0400

    Complete worldToDeque

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,49 +1,11 @@
 public class Palindrome {
 
     public Deque<Character> wordToDeque(String word) {
-
-        Deque<Character> charValue = new Deque<>() {
-            @Override
-            public void addFirst(Character item) {
-
-            }
-
-            @Override
-            public void addLast(Character item) {
-
-            }
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public void printDeque() {
-
-            }
-
-            @Override
-            public Character removeFirst() {
-                return null;
-            }
-
-            @Override
-            public Character removeLast() {
-                return null;
-            }
-
-            @Override
-            public Character get(int index) {
-                return null;
-            }
-        };
-
+        LinkedListDeque<Character> charValue = new LinkedListDeque<>();
         for (int i = 0; i < word.length(); i++) {
             char temp = word.charAt(i);
             charValue.addLast(temp);
         }
-
         return charValue;
     }
 }

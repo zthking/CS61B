a80c9175e1fdae29a5580d5cdf397b14f6a080a5
zthking <tzhang017@gmail.com>
2019-06-23 13:34:17 -0400

    Revise constructor

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -6,61 +6,37 @@ import edu.princeton.cs.introcs.StdStats;
 public class PercolationStats {
 
     private int times;
-    private int boundary;
     private double[] frictions;
-    private Percolation[] p;
 
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 || T <= 0) {
             throw new IllegalArgumentException();
         }
-        boundary = N;
-        times = T;
-        p = new Percolation[T];
-        frictions = new double[times];
-        Percolation pMaker = pf.make(N);
-        runPercolation(pMaker);
-        /*
-        for (int i = 0; i < T; i += 1) {
-            p[i] = pf.make(N);
-        }
-         */
-    }
 
-    private void runPercolation(Percolation pMaker) {
+        times = T;
+        Percolation[] p = new Percolation[T];
+        frictions = new double[T];
+
         int row;
         int col;
-        for (int i = 0; i < times; i += 1) {
-            p[i] = pMaker;
+        for (int i = 0; i < T; i += 1) {
+            p[i] = pf.make(N);
             while (!p[i].percolates()) {
-                row = StdRandom.uniform(0, boundary);
-                col = StdRandom.uniform(0, boundary);
+                row = StdRandom.uniform(0, N);
+                col = StdRandom.uniform(0, N);
                 if (!p[i].isOpen(row, col)) {
                     p[i].open(row, col);
                 }
             }
-            frictions[i] = (double) p[i].numberOfOpenSites() / boundary / boundary;
+            frictions[i] = (double) p[i].numberOfOpenSites() / (N * N);
         }
     }
 
     public double mean() {
-        /*
-        if (frictions[0] != 0.0d) {
-            return StdStats.mean(frictions);
-        }
-        runPercolation();
-         */
         return StdStats.mean(frictions);
     }
 
     public double stddev() {
-        /*
-        if (frictions[0] != 0.0d) {
-            return StdStats.stddev(frictions);
-        }
-        runPercolation();
-
-         */
         return StdStats.stddev(frictions);
     }
 

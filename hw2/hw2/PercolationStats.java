f01a79b03e659f90b36fdb1e2c3ddcb9ce93be07
zthking <tzhang017@gmail.com>
2019-06-23 10:37:26 -0400

    Update Constructor

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -5,10 +5,11 @@ import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
 
-    private Percolation p;
+    //private Percolation p;
     private int times;
     private int boundary;
     private int[] totalRuns;
+    private Percolation[] p;
 
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 || T <= 0) {
@@ -16,10 +17,15 @@ public class PercolationStats {
         }
         boundary = N;
         times = T;
-        p = pf.make(N);
+        p = new Percolation[T];
+        for (int i = 0; i < T; i += 1) {
+            p[i] = pf.make(N);
+        }
+        //p = pf.make(N);
         totalRuns = new int[times];
     }
 
+    /*
     private void runPercolation() {
         for (int i = 0; i < times; i += 1) {
             do {
@@ -28,13 +34,28 @@ public class PercolationStats {
             totalRuns[i] = p.numberOfOpenSites();
         }
     }
+*/
+    private void runPercolation() {
+        for (int i = 0; i < times; i += 1) {
+            do {
+                p[i].open(StdRandom.uniform(0, boundary), StdRandom.uniform(0, boundary));
+            } while (p[i].percolates());
+            totalRuns[i] = p[i].numberOfOpenSites();
+        }
+    }
 
     public double mean() {
+        if (totalRuns[0] != 0) {
+            return StdStats.mean(totalRuns);
+        }
        runPercolation();
        return StdStats.mean(totalRuns);
     }
 
     public double stddev() {
+        if (totalRuns[0] != 0) {
+            return StdStats.stddev(totalRuns);
+        }
         runPercolation();
         return StdStats.stddev(totalRuns);
     }
